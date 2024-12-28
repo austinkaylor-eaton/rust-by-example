2b4a33735342c1e5f864b9e56e5c2205015912ce
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-27 21:57:56 -0500

    Chapter 2 Project - Setting up the guessing game to read user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,28 @@
+/*
+    The standard library is the set of features that Rust gives you out of the box.
+    It’s packaged with the Rust language for your use.
+    The standard library includes common Rust types, like vectors, strings, and hash maps,
+    as well as traits that represent common behavior, like the iterator trait that enables
+    you to use .iter() on vectors to loop through them.
+    The standard library also includes Rust’s built-in macros, the core types and traits in Rust,
+    the standard library API reference, and the Rust Prelude, which is a small set of items
+    that Rust automatically imports into every Rust program.
+    
+     std::io gives you the ability to accept user input and print text to the screen
+ */
+use std::io; 
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // created a mutable variable that is currently bound to a new, empty instance of a String
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line"); // crashes the program if an error occurs
+
+    println!("You guessed: {}", guess); // {} is a placeholder for the value of the variable
 }
