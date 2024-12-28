27808a8c06e7613344c816670f6804b815078ef2
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-27 22:57:29 -0500

    Chapter 1 - Hello World

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,18 +1,18 @@
 /*
-    The standard library is the set of features that Rust gives you out of the box.
-    It’s packaged with the Rust language for your use.
-    The standard library includes common Rust types, like vectors, strings, and hash maps,
-    as well as traits that represent common behavior, like the iterator trait that enables
-    you to use .iter() on vectors to loop through them.
-    The standard library also includes Rust’s built-in macros, the core types and traits in Rust,
-    the standard library API reference, and the Rust Prelude, which is a small set of items
-    that Rust automatically imports into every Rust program.
-    
-     std::io gives you the ability to accept user input and print text to the screen
- */
-use std::io;
+   The standard library is the set of features that Rust gives you out of the box.
+   It’s packaged with the Rust language for your use.
+   The standard library includes common Rust types, like vectors, strings, and hash maps,
+   as well as traits that represent common behavior, like the iterator trait that enables
+   you to use .iter() on vectors to loop through them.
+   The standard library also includes Rust’s built-in macros, the core types and traits in Rust,
+   the standard library API reference, and the Rust Prelude, which is a small set of items
+   that Rust automatically imports into every Rust program.
+
+    std::io gives you the ability to accept user input and print text to the screen
+*/
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
